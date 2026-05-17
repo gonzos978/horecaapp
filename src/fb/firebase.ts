f2cdd3d274c24766ad026e98501727d09345be35
adnan <gdev@netzonelab.com>
2026-05-17 18:26:21 +0200

    update fb

--- src/fb/firebase.ts
+++ src/fb/firebase.ts
@@ -1,24 +1,76 @@
 import { initializeApp } from "firebase/app";
+
 import { getAuth } from "firebase/auth";
-import { getFirestore, serverTimestamp } from "firebase/firestore";
+
+import {
+    getFirestore,
+    serverTimestamp
+} from "firebase/firestore";
+
 import { getStorage } from "firebase/storage";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyAbEVmo0BWoRZwAR9bv0Tyf0xP4dOEC1pU",
-    authDomain: "horecaapp-e16cf.firebaseapp.com",
-    projectId: "horecaapp-e16cf",
-    storageBucket: "horecaapp-e16cf.firebasestorage.app",
-    messagingSenderId: "1033337228120",
-    appId: "1:1033337228120:web:549fee0fe3dd656845332d",
-    measurementId: "G-Q36V58CL8C"
-}
+import {
+    getFunctions,
+    connectFunctionsEmulator
+} from "firebase/functions";
 
+const firebaseConfig = {
+
+    apiKey: "AIzaSyAbEVmo0BWoRZwAR9bv0Tyf0xP4dOEC1pU",
+
+    authDomain: "horecaapp-e16cf.firebaseapp.com",
+
+    projectId: "horecaapp-e16cf",
+
+    storageBucket: "horecaapp-e16cf.firebasestorage.app",
+
+    messagingSenderId: "1033337228120",
+
+    appId: "1:1033337228120:web:549fee0fe3dd656845332d",
+
+    measurementId: "G-Q36V58CL8C"
+};
 
 const app = initializeApp(firebaseConfig);
 
+/**
+ * AUTH
+ */
+
 export const auth = getAuth(app);
+
+/**
+ * FIRESTORE
+ */
+
 export const db = getFirestore(app);
+
 export const firestore = getFirestore(app);
+
+/**
+ * STORAGE
+ */
+
 export const storage = getStorage(app);
+
+/**
+ * FUNCTIONS
+ */
+
+export const functions = getFunctions(app);
+
+/**
+ * LOCAL EMULATOR
+ * OPTIONAL
+ */
+
+if (window.location.hostname === "localhost") {
+
+    connectFunctionsEmulator(
+        functions,
+        "127.0.0.1",
+        5001
+    );
+}
 
 export { serverTimestamp };
